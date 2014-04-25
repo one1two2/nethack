49f2c63906bc7925f021996669f374f2775ac95a
One1Two2 <konbaranowski@gmail.com>
2014-04-25 22:21:31 +0200

    Logowanie przez fejsa

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
+
+require ('facebook-sdk/src/facebook.php');
+    
+$config = array(
+    'appId' => '250002738518098',
+    'secret' => 'a63c2d788c57c79bcd46026f670423f5',
+    'allowSignedRequest' => false
+);
+
+$facebook = new Facebook($config);
+$user_id = $facebook->getUser();
+//var_dump($user_id);
+    
+if($user_id) {
+    try {
+        $user_profile = $facebook->api('/me','GET');
+      } catch(FacebookApiException $e) {
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+}
+
 include 'views/index.php';
 ?>
--- views/index.php
+++ views/index.php
@@ -18,6 +18,29 @@
             });
             
         </script>
+        <div id="fb-root"></div>
+        <script>               
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId: '<?php echo $facebook->getAppID() ?>', 
+          cookie: true, 
+          xfbml: true,
+          oauth: true
+        });
+        FB.Event.subscribe('auth.login', function(response) {
+          window.location.reload();
+        });
+        FB.Event.subscribe('auth.logout', function(response) {
+          window.location.reload();
+        });
+      };
+      (function() {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol +
+          '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
         
         <div id="page">
 
@@ -26,7 +49,7 @@
 
         </div>
         <div id="panel">
-
+            <div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true"></div>
         </div>
         <div class="clear"></div>
     </div>
