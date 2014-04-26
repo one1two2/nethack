bd44a35a7b7cd83240f3c6932ebde30ad53ea96c
One1Two2 <konbaranowski@gmail.com>
2014-04-26 10:41:11 +0200

    get na event

--- ajax/event.php
+++ ajax/event.php
@@ -1,14 +1,18 @@
 <?php
 require '../init.php';
 
-$eid = 5;
-
-$sql = 'SELECT * FROM event AS e LEFT JOIN location AS l ON e.location_id=l.id WHERE e.eid = '.$eid.'';
-$result=mysql_query($sql);
-$event = null;
-while($w=mysql_fetch_assoc($result)){
+if(array_key_exists('eid', $_GET)===true){
+    
+    $eid=filter_input(INPUT_GET, 'eid', FILTER_VALIDATE_INT);
+    
+    $sql = 'SELECT * FROM event AS e LEFT JOIN location AS l ON e.location_id=l.id WHERE e.eid = '.$eid.'';
+    $result=mysql_query($sql);
+    $event = null;
+    while($w=mysql_fetch_assoc($result)){
         $event=$w;
         echo getView('event', $event);
+    }
+
 }
 //var_dump($sql);
 ?>
